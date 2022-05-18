91539aacd2663f5e787263f1874812cab7856791
Margaret Britton <margaret.ann.britton@gmail.com>
2022-05-18 16:55:02 -0500

    About Text, Part 2

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,10 @@ const About = () => {
         <h3>Margaret Britton</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lectus magna fringilla urna porttitor rhoncus dolor. Penatibus et magnis dis parturient montes nascetur ridiculus mus. At quis risus sed vulputate odio ut enim blandit volutpat.</p>
         <p>Enim eu turpis egestas pretium aenean pharetra magna. Augue eget arcu dictum varius duis at consectetur lorem donec. Tempus urna et pharetra pharetra massa massa ultricies. Parturient montes nascetur ridiculus mus. Facilisi cras fermentum odio eu feugiat.</p>
+        <h5>Current Frameworks / Libraries / Technologies: </h5>
+        <p>HTML  |  CSS  |  JavaScript <br /> React.js  |  React Native  |  VS Code
+        </p>
+        <button>Resum&eacute;</button>
     </section>
   );
 }
