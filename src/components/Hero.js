640e125cadf272b51f906fb923539e218378df17
Margaret Britton <margaret.ann.britton@gmail.com>
2022-05-17 22:52:37 -0500

    Hero Text

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section>
+        <p>Hi, my name is</p>
+        <h1>Margaret Britton</h1>
+        <p>I'm a <strong>front&ndash;end web developer</strong> with a background in <strong>music theory</strong> and a passion for making the web both <strong>beautiful</strong> and <strong>functional</strong>.</p>
+    </section>
+  );
+}
+
+export default Hero;
