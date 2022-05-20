529d067e8d04e1cb4cfd281aeaf7cb7c34f62e41
Margaret Britton <margaret.ann.britton@gmail.com>
2022-05-19 21:56:44 -0500

    Hero Styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <section>
-        <p>Hi, my name is</p>
+    <div className="hero-container">
+      <div className="hero-content">
+        <p className="hero-intro">Hi, my name is</p>
         <h1>Margaret Britton</h1>
-        <p>I'm a <strong>front&ndash;end web developer</strong> with a background in <strong>music theory</strong> and a passion for making the web both <strong>beautiful</strong> and <strong>functional</strong>.</p>
-    </section>
+        <p className="hero-body">I'm a <strong>front&ndash;end web developer</strong> with a background in <strong>music theory</strong> and a passion for making the web both <strong>functional</strong> and <strong>beautiful</strong>.</p>
+      </div>
+    </div>
   );
 }
 
