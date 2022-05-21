0caad4399b6dd996aae88eb57f80d541abf382df
Margaret Britton <margaret.ann.britton@gmail.com>
2022-05-21 13:23:05 -0500

    Project Icon Styling with MUI

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LaunchIcon from '@mui/icons-material/Launch';
+import IconButton from '@mui/material/IconButton';
 
 const Projects = () => {
   return (
@@ -10,10 +11,19 @@ const Projects = () => {
         <div className="project-container">
             <h4>Anywhere Montessori School</h4>
             <p className="project-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div>
-                <GitHubIcon /> 
-                <LaunchIcon />
-                <p>HTML  |  CSS  |  Bootstrap</p>           
+            <div className="bottom-project-container">
+                <div>
+                    <IconButton>
+                      <GitHubIcon fontSize="large" sx={{ color: "#fff", "&:hover": { color: "darkgray" } }} />  
+                    </IconButton>
+                    <IconButton>
+                        <LaunchIcon fontSize="large"
+                        sx={{ color: "#fff", "&:hover": { color: "darkgray" } }} 
+                        />   
+                    </IconButton>   
+                </div>
+                
+                <span className="project-tech">HTML  |  CSS  |  Bootstrap</span>           
             </div>
         </div>
         <img className="project-img" src='images/anywhereMontessoriSchool.jpg' alt='Anywhere Montessori School Webpage' />
@@ -21,10 +31,19 @@ const Projects = () => {
         <div className="project-container">
             <h4>Toy Lending Library</h4>
             <p className="project-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <div>
-                <GitHubIcon /> 
-                <LaunchIcon />
-                <p>React.js  |  CSS  |  etc.</p>           
+            <div className="bottom-project-container">
+                <div>
+                <IconButton>
+                      <GitHubIcon fontSize="large" sx={{ color: "#fff", "&:hover": { color: "darkgray" } }} />  
+                    </IconButton>
+                    <IconButton>
+                        <LaunchIcon fontSize="large"
+                        sx={{ color: "#fff", "&:hover": { color: "darkgray" } }} 
+                        />   
+                    </IconButton>  
+                </div>
+                 
+                <span className="project-tech">React  |  CSS  |  Reactstrap</span>           
             </div>
         </div>
         <img className="project-img" src='images/toyLendingLibrary.jpg' alt='Toy Lending Library Webpage' />
