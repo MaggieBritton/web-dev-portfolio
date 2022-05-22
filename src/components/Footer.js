148bc9f93a3265e3087ddfef01c9c2f466bbc8e7
Margaret Britton <margaret.ann.britton@gmail.com>
2022-05-21 23:00:52 -0500

    Footer Styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import IconButton from '@mui/material/IconButton';
 
 const Footer = () => {
   return (
-    <section>
-        <p>connect with me</p>
-        <div>
-            <GitHubIcon />
-            <LinkedInIcon />
+    <div className="footer-container">
+        <p className="connect">connect with me</p>
+        <div className="footer-icon-container">
+            <IconButton>
+              <GitHubIcon fontSize="large" sx={{ color: "#fff", "&:hover": { color: "darkgray" } }} />
+            </IconButton>
+            <IconButton>
+              <LinkedInIcon fontSize="large" sx={{ color: "#fff", "&:hover": { color: "darkgray" } }} />
+            </IconButton>  
         </div>
-        <small>&copy; 2022 M. Britton Designs</small>
-    </section>
+        <small className="copyright">&copy; 2022 M. Britton Designs</small>
+    </div>
   );
 }
 
